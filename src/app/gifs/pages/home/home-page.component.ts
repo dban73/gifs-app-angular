1aa953693ba562c6a7d0e73cf294b30c10e8e9a8
Alejandro <dban095@hotmail.com>
2023-12-29 18:36:58 -0400

    gifs app

---
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs-service.service';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {
+  constructor(private gifsService: GifsService) {}
+
+  get gifs() {
+    return this.gifsService.gifList;
+  }
+}
